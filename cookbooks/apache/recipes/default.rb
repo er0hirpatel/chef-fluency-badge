b04134901f52ed0f0c650c61a1b1e19e47146c41
er0hirpatel <er.hirpatel@gmail.com>
2017-01-30 17:37:01 +0000

    Added motd and changed apache default for node attributes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,8 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+	if node['platform_family'] == "rhel"
+		package = "httpd"
+	elseif node['platform_family'] == "debian"
+		pachage = "apache2"
+	end
 	package 'apache2' do
-		package_name 'httpd'
+		package_name package 
 		action :install
 	end
 
